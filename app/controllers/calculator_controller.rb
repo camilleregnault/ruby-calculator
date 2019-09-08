6c0fbe8e2200c4e8403d76ca090b0461da6ddf81
Camille Regnault <c.regnault.pro@gmail.com>
2019-09-08 19:23:43 +0200

    create calculate method

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -10,7 +10,15 @@ class CalculatorController < ApplicationController
     arguments = parse_argument(input_calc)
     case arguments[:operator]
     when '+'
-      addition(input_a, input_b)
+      addition(arguments[:input_a], arguments[:input_b])
+    when '-'
+      subtraction(arguments[:input_a], arguments[:input_b])
+    when '*'
+      multiplication(arguments[:input_a], arguments[:input_b])
+    when '/'
+      division(arguments[:input_a].to_f, arguments[:input_b])
+    when '%'
+      modulo(arguments[:input_a], arguments[:input_b])
     end
   end
 
@@ -24,27 +32,30 @@ class CalculatorController < ApplicationController
       raise ArgumentError, 'Arguments to be [Float, operator, Float]'
     end
 
-    split_argument.each_with_index { |arg, index| arguments[INPUT_KEYS[index]] = arg }
+    split_argument.each_with_index do |arg, index|
+      arguments[INPUT_KEYS[index]] = arg if index == 1
+      arguments[INPUT_KEYS[index]] = arg.to_f if index.zero? || index == 2
+    end
     arguments
   end
 
   def addition(a, b)
-    (a.to_f + b.to_f).round(2)
+    (a + b).round(2)
   end
 
   def subtraction(a, b)
-    (a.to_f - b.to_f).round(2)
+    (a - b).round(2)
   end
 
   def multiplication(a, b)
-    a.zero? || b.zero? ? 0 : (a.to_f * b.to_f).round(2)
+    a.zero? || b.zero? ? 0 : (a * b).round(2)
   end
 
   def division(a, b)
-    a.zero? || b.zero? ? 0 : (a.to_f / b).round(2)
+    a.zero? || b.zero? ? 0 : (a / b).round(2)
   end
 
   def modulo(a, b)
-    a.zero? || b.zero? ? 0 : a.to_f % b.to_f
+    a.zero? || b.zero? ? 0 : a % b
   end
 end
--- spec/controllers/calculator_controller_spec.rb
+++ spec/controllers/calculator_controller_spec.rb
@@ -16,9 +16,9 @@ RSpec.describe CalculatorController, type: :controller do
 
     it 'retruns parsed input string' do
       expect(cal.send(:parse_argument, '1 + 3')).to eq(
-        input_a: '1',
+        input_a: 1.0,
         operator: '+',
-        input_b: '3'
+        input_b: 3.0
       )
     end
   end
@@ -27,65 +27,65 @@ RSpec.describe CalculatorController, type: :controller do
     it 'returns an addition result' do
       expect(cal.send(:addition, 1.5, 1.6)).to eq(3.1)
       expect(cal.send(:addition, 1.333333, 1.121212)).to eq(2.45)
-      expect(cal.send(:addition, -1, 3)).to eq(2)
-      expect(cal.send(:addition, 1, 0)).to eq(1)
-      expect(cal.send(:addition, -1, -30)).to eq(-31)
+      expect(cal.send(:addition, -1.0, 3.0)).to eq(2)
+      expect(cal.send(:addition, 1.0, 0)).to eq(1)
+      expect(cal.send(:addition, -1.0, -30.0)).to eq(-31)
       expect(cal.send(:addition,
-                      9_999_999_999,
-                      9_999_999_999)).to eq(19_999_999_998)
+                      9_999_999_999.0,
+                      9_999_999_999.0)).to eq(19_999_999_998)
     end
 
     it 'returns a subtraction result' do
-      expect(cal.send(:subtraction, 1.5, 3)).to eq(-1.5)
-      expect(cal.send(:subtraction, -1, 3)).to eq(-4)
-      expect(cal.send(:subtraction, 1, 0)).to eq(1)
-      expect(cal.send(:subtraction, -1, -30)).to eq(29)
+      expect(cal.send(:subtraction, 1.5, 3.0)).to eq(-1.5)
+      expect(cal.send(:subtraction, -1.0, 3.0)).to eq(-4)
+      expect(cal.send(:subtraction, 1.0, 0)).to eq(1)
+      expect(cal.send(:subtraction, -1.0, -30.0)).to eq(29)
       expect(cal.send(:subtraction,
-                      9_999_999_999,
-                      -9_999_999_999)).to eq(19_999_999_998)
+                      9_999_999_999.0,
+                      -9_999_999_999.0)).to eq(19_999_999_998)
     end
 
     it 'returns a multiplication result' do
-      expect(cal.send(:multiplication, 1.5, 3)).to eq(4.5)
-      expect(cal.send(:multiplication, -1, 3)).to eq(-3)
-      expect(cal.send(:multiplication, 1, 0)).to eq(0)
-      expect(cal.send(:multiplication, 1, 0.5)).to eq(0.5)
-      expect(cal.send(:multiplication, -1, -30)).to eq(30)
+      expect(cal.send(:multiplication, 1.5, 3.0)).to eq(4.5)
+      expect(cal.send(:multiplication, -1.0, 3.0)).to eq(-3)
+      expect(cal.send(:multiplication, 1.0, 0)).to eq(0)
+      expect(cal.send(:multiplication, 1.0, 0.5)).to eq(0.5)
+      expect(cal.send(:multiplication, -1.0, -30.0)).to eq(30)
       expect(cal.send(:multiplication,
-                      9_999_999_999,
-                      9_999_999_999)).to eq(9.999999998e+19)
+                      9_999_999_999.0,
+                      9_999_999_999.0)).to eq(9.999999998e+19)
     end
 
     it 'returns a division result' do
-      expect(cal.send(:division, 1, 3)).to eq(0.33)
-      expect(cal.send(:division, -1, 3)).to eq(-0.33)
-      expect(cal.send(:division, 1, 0)).to eq(0)
-      expect(cal.send(:division, 1, 0.5)).to eq(2)
-      expect(cal.send(:division, -1, -30)).to eq(0.03)
+      expect(cal.send(:division, 1.0, 3.0)).to eq(0.33)
+      expect(cal.send(:division, -1.0, 3.0)).to eq(-0.33)
+      expect(cal.send(:division, 1.0, 0)).to eq(0)
+      expect(cal.send(:division, 1.0, 0.5)).to eq(2)
+      expect(cal.send(:division, -1.0, -30.0)).to eq(0.03)
       expect(cal.send(:division,
-                      9_999_999_999,
-                      9_999_999_999)).to eq(1)
+                      9_999_999_999.0,
+                      9_999_999_999.0)).to eq(1)
     end
 
     it 'returns a modulo result' do
-      expect(cal.send(:modulo, 1, 3)).to eq(1)
-      expect(cal.send(:modulo, -1, 3)).to eq(2)
-      expect(cal.send(:modulo, 1, 0)).to eq(0)
-      expect(cal.send(:modulo, 1, 0.5)).to eq(0)
-      expect(cal.send(:modulo, -1, -30)).to eq(-1)
+      expect(cal.send(:modulo, 1.0, 3.0)).to eq(1)
+      expect(cal.send(:modulo, -1.0, 3.0)).to eq(2)
+      expect(cal.send(:modulo, 1.0, 0)).to eq(0)
+      expect(cal.send(:modulo, 1.0, 0.5)).to eq(0)
+      expect(cal.send(:modulo, -1.0, -30.0)).to eq(-1)
       expect(cal.send(:modulo,
-                      9_999_999_999,
-                      9_999_999_999)).to eq(0)
+                      9_999_999_999.0,
+                      9_999_999_999.0)).to eq(0)
     end
   end
 
   describe '#calculate' do
-    # it 'returns a message when string is emtpy' do 
-    #   expect(count_letters('')).to eq('Duh Empty line!')
-    # end
-    
-    # it 'returns a result for a given argument' do
-    #   expect(count_letters(@string)).to eq(@string_count)
-    # end
+    it 'returns a result for a given argument' do
+      expect(cal.calculate('1 + 50.5')).to eq(51.5)
+      expect(cal.calculate('10 - 50.5')).to eq(-40.5)
+      expect(cal.calculate('10 * 0.5')).to eq(5)
+      expect(cal.calculate('100 / 50.5')).to eq(1.98)
+      expect(cal.calculate('5 % 50')).to eq(5)
+    end
   end
 end
